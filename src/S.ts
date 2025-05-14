7f3e6e405c97382558e251f6bf5a16c367d50c78
Maurycy Zarzycki <maurycy@evidentlycube.com>
2025-05-14 12:13:38 +0200

    1.6.6 release

--- src/S.ts
+++ src/S.ts
@@ -3,7 +3,7 @@ import {HoldOptions} from "./platform/PlatformSpecific";
 
 export const S = {
 	DEBUG: false,
-	EngineVersion: 'V1.6.5-JS',
+	EngineVersion: 'V1.6.6-JS',
 	currentHoldOptions: undefined as (HoldOptions | undefined),
 
 	pagedHoldOptions: [] as HoldOptions[][],
